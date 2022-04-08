52f2276ec7f40c5a0a6d9ab32485761e789bfa13
ZealousNarwhal <mookman909@gmail.com>
2022-04-08 10:54:48 -0400

    updated file structure to support input/output files better

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import sys
+import os
+
+# get the root path of our project
+ROOT_DIR = os.path.abspath(os.curdir)
 
 def main():
     args = sys.argv[1:]
     for arg in args:
-        with open('output.txt', 'w') as f:
-            f.write(read_file(arg))
+        file_path = f"{ROOT_DIR}\input\{arg}"
+        file_text = read_file(file_path)
+        if file_text:
+            file_path = f"{ROOT_DIR}\output\{arg}"
+            write_file(file_path, file_text)
 
 def read_file(file):
-    with open(file, 'r') as f:
-        text = f.read().replace('\n', ' ')
-    return text
+    try:
+        with open(file, 'r') as f:
+            text = f.read().replace('\n', ' ')
+        return text
+    except:
+        print(f"Failed to read file {file}.")
+        return False
+
+
+def write_file(file, text):
+    try:
+        with open(file, 'w') as f:
+            f.write(text)
+            return True
+    except:
+        print(f"Failed to write to file {file}.")
+        return False
 
 
 if __name__ == '__main__':
