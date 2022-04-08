94ba789d798f54fab483732468b105d654257e16
ZealousNarwhal <mookman909@gmail.com>
2022-04-08 11:11:36 -0400

    tests now log multiple errors

--- test/test_main.py
+++ test/test_main.py
@@ -2,5 +2,14 @@ from main import read_file
 import os
 
 def test_read_file():
+    errors = []
     os.chdir("test")
-    assert (read_file("testfile.txt") == "Hello World! How are you?")
+
+    # Does read return false if given an improper file path?
+    if not(read_file("") == False):
+        errors.append("read_file failed to return False with an incorrect file path.")
+
+    if not(read_file("testfile.txt") == "Hello World! How are you?"):
+        errors.append("read_file failed to parse correctly.")
+
+    assert not errors, errors
